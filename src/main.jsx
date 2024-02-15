e42ed0c902dddfe967252769d231430419cf4fe8
Amit ASJ <amitsisodiyajadaun@gmail.com>
2024-02-16 00:05:30 +0530

    store file add

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store } from "./redux/Store.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
